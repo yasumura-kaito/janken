0375a70cdf44765fd9aee207c4fafbf92221e3f2
yasumura-kaito <e1b19100@oit.ac.jp>
2021-09-28 15:35:53 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0346/kaizi/janken/JankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z0346.kaizi.janken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0346/kaizi/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0346.kaizi.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
