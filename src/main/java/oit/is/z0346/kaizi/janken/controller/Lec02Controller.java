e6ecb6d1627828b24198f0fab05fe9e052081ca1
yasumura-kaito <e1b19100@oit.ac.jp>
2021-10-11 21:54:38 +0900

    ユーザ入力をし、lec02.htmlに表示する処理の実装

---
+++ src/main/java/oit/is/z0346/kaizi/janken/controller/Lec02Controller.java
@@ -0,0 +1,24 @@
+package oit.is.z0346.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Lec02Controller{
+
+  @GetMapping("/lec02")
+  public String lec02() {
+    return "lec02.html";
+  }
+
+  @PostMapping("/lec02")
+  public String lec02(@RequestParam String name, ModelMap model) {
+    String user_name = name;
+    model.addAttribute("name", user_name);
+    return "lec02.html";
+  }
+}
---
+++ src/main/java/oit/is/z0346/kaizi/janken/model/janken.java
@@ -0,0 +1,15 @@
+package oit.is.z0346.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+public class janken{
+  ArrayList<String> strList = new ArrayList<>();
+
+  public ArrayList<String> getNumList() {
+    return strList;
+  }
+
+  public void setNumList(ArrayList<String> numList) {
+    this.strList = strList;
+  }
+}
