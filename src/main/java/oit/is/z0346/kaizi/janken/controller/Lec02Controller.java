c329704de6a90c15195df48d0cc1f5623582a941
yasumura-kaito <e1b19100@oit.ac.jp>
2021-10-18 22:48:50 +0900

    多数のユーザーがログインできるように変更

--- src/main/java/oit/is/z0346/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0346/kaizi/janken/controller/Lec02Controller.java
@@ -1,19 +1,31 @@
 package oit.is.z0346.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0346.kaizi.janken.model.janken;
+import oit.is.z0346.kaizi.janken.model.Room;
 
 @Controller
 public class Lec02Controller{
 
+  @Autowired
+  private Room room;
+
   @GetMapping("/lec02")
-  public String lec02() {
+  public String lec02(ModelMap model, Principal prin) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("login_user", loginUser);
+    model.addAttribute("room", this.room);
     return "lec02.html";
   }
 
---
+++ src/main/java/oit/is/z0346/kaizi/janken/security/jankenAuthConfiguration.java
@@ -0,0 +1,37 @@
+package oit.is.z0346.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class jankenAuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("p@ss")).roles("USER");
+    auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder().encode("c0rd")).roles("USER");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // Spring Securityのフォームを利用してログインを行う
+    http.formLogin();
+
+    http.authorizeRequests().antMatchers("/lec02/**").authenticated();
+
+    http.logout().logoutSuccessUrl("/");
+
+  }
+}
