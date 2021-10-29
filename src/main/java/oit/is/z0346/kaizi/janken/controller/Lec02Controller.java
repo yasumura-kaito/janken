e759a9203a3ac89186cda5ac0a9061cd2611d89c
yasumura-kaito <e1b19100@oit.ac.jp>
2021-10-29 15:19:24 +0900

    じゃんけんの仕様を変更

--- src/main/java/oit/is/z0346/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0346/kaizi/janken/controller/Lec02Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import oit.is.z0346.kaizi.janken.model.janken;
+import oit.is.z0346.kaizi.janken.model.Janken;
 import oit.is.z0346.kaizi.janken.model.Room;
 import oit.is.z0346.kaizi.janken.model.UserMapper;
 import oit.is.z0346.kaizi.janken.model.User;
@@ -54,63 +54,21 @@ public class Lec02Controller{
     return "match.html";
   }
 
-  @GetMapping("/lec02A")
-  public String lec02A(ModelMap model) {
-    String your_hand = "Gu";
-    String cpu_hand = "Gu";
-    String result = "Draw!";
+  @GetMapping("/battle")
+  public String battle(@RequestParam Integer hand, ModelMap model) {
+    Janken janken1 = new Janken(hand);
+    model.addAttribute("your_hand", janken1.getYourHand());
+    model.addAttribute("cpu_hand", janken1.getCpuHand());
+    model.addAttribute("result", janken1.getResult());
     ArrayList<Match> matches = matchMapper.selectAll();
     int cnt = matches.size();
     Match match2 = new Match();
     match2.setId(cnt + 1);
     match2.setUser1(2);
     match2.setUser2(1);
-    match2.setUser1Hand(your_hand);
-    match2.setUser2Hand(cpu_hand);
+    match2.setUser1Hand(janken1.getYourHand());
+    match2.setUser2Hand(janken1.getCpuHand());
     matchMapper.insertMatch(match2);
-    model.addAttribute("your_hand", your_hand);
-    model.addAttribute("cpu_hand", cpu_hand);
-    model.addAttribute("result", result);
-    return "match.html";
-  }
-
-  @GetMapping("/lec02B")
-  public String lec02B(ModelMap model) {
-    String your_hand = "Choki";
-    String cpu_hand = "Gu";
-    String result = "Lose!";
-    ArrayList<Match> matches = matchMapper.selectAll();
-    int cnt = matches.size();
-    Match match2 = new Match();
-    match2.setId(cnt + 1);
-    match2.setUser1(2);
-    match2.setUser2(1);
-    match2.setUser1Hand(your_hand);
-    match2.setUser2Hand(cpu_hand);
-    matchMapper.insertMatch(match2);
-    model.addAttribute("your_hand", your_hand);
-    model.addAttribute("cpu_hand", cpu_hand);
-    model.addAttribute("result", result);
-    return "match.html";
-  }
-
-  @GetMapping("/lec02C")
-  public String lec02C(ModelMap model) {
-    String your_hand = "Pa";
-    String cpu_hand = "Gu";
-    String result = "Win!";
-    ArrayList<Match> matches = matchMapper.selectAll();
-    int cnt = matches.size();
-    Match match2 = new Match();
-    match2.setId(cnt + 1);
-    match2.setUser1(2);
-    match2.setUser2(1);
-    match2.setUser1Hand(your_hand);
-    match2.setUser2Hand(cpu_hand);
-    matchMapper.insertMatch(match2);
-    model.addAttribute("your_hand", your_hand);
-    model.addAttribute("cpu_hand", cpu_hand);
-    model.addAttribute("result", result);
     return "match.html";
   }
 
